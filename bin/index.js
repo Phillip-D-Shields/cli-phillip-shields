4f5d0e8fea84b5632e50559bdae0ca9566cf6164
Phillip <phillip.d.shields1382@gmaIl.com>
2020-03-15 20:02:33 +1300

    updated submission text
    add an add feature

--- bin/index.js
+++ bin/index.js
@@ -5,9 +5,12 @@ const axios = require("axios");
 
 let className;
 
-const INFO = {
+// setup option to add entries
+let INFO = {
   des01:
     "Gitbook activities submitted during week01 can be found here: https://phillip-shields.gitbook.io/des501/topic-1/activity-1.1",
+  des02:
+    "Gitbook activities submitted during week01 can be found here: https://phillip-shields.gitbook.io/des501/topic-2/activity-2.1",
   sdv00:
     "the readme.md for this lab can be found here: https://github.com/Phillip-D-Shields/sdv-labs/blob/master/README0.md",
   sdv01:
@@ -27,19 +30,18 @@ const options = yargs
   })
   .option("s", {
     alias: "select",
-    describe:
-      "-s <class+weekNumber>",
+    describe: "-s <class+weekNumber>",
     type: "string"
+  })
+  .option("l", {
+    alias: "list",
+    describe: "-l list all current submissions in database"
   }).argv;
 
 //   entering name is required in options
-const greeting = `Hello, ${options.name}`;
-console.log(greeting);
+const greeting = () => `Hello, ${options.name}`;
 
-if (options.select) {
-  displaySelection();
-}
-
+// display -s selections
 function displaySelection() {
   // show work
   console.log(`lookin' for items related to ${options.select}...`);
@@ -55,6 +57,13 @@ ${INFO[key]}`);
   }
 }
 
+function displayAll() {
+  console.log("Here are all the current submissions available");
+  // console log all available submissions
+  console.log(Object.keys(INFO));
+}
+
+// random unwanted advice given at the end
 function displayAdvice() {
   // throw some advice in there for ali
   axios.get("https://api.adviceslip.com/advice").then(res => {
@@ -63,6 +72,14 @@ function displayAdvice() {
     
 ${options.name}, here is some valuable advice for you today: ${advice}`);
   });
+}
+
+if (options.select) {
+  displaySelection();
+}
+
+if (options.list) {
+  displayAll();
 }
 
 displayAdvice();
